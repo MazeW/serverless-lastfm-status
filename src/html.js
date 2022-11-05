381f4278c42d6e8824a98019c7d70ab508b3c310
Maze <Maze@Endymion>
2022-11-05 18:58:14 +0200

    inital commit

---
+++ src/html.js
@@ -0,0 +1,40 @@
+module.exports.main = function () {
+    return template(`
+    <h1>Last.fm status</h1>
+    <p>You can use <code>/api/username</code> to get a json response and <code>/user/username</code> to get html reponse.
+    Example: <code>/user/i0l</code></p>
+    `);
+}
+
+module.exports.info = function (info) {
+    return template(`
+    <h1>${info.user} ${(info.playing ? "is listening to" : "last listened to")}</h1>
+    <div><img style="border-style:solid;" src="${info.image}"></div>
+    <p><b>${info.artist}</b> — ${info.name}</p>
+    `);
+}
+
+module.exports.error = function (msg) {
+    return template(`
+    <h1>Whoops</h1>
+    <p>${msg}</p>
+    `);
+}
+
+
+const template = (body) => {
+    return `<!DOCTYPE html>
+    <html>
+    <head>
+    <title>
+    Last.fm most recent</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This page displays the song user is listening or last listened to on Last.fm">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/spcss@0.8.0">
+    </head>
+    <body>
+        ${body}
+    </body>
+    </html>
+    `
+}
---
+++ src/index.js
@@ -0,0 +1,52 @@
+const html = require('./html.js');
+
+const initJson = {
+  headers: {
+    'content-type': 'application/json;charset=UTF-8',
+  },
+};
+
+const initHtml = {
+  headers: {
+    'content-type': 'text/html;charset=UTF-8',
+  },
+};
+
+
+async function getSongInfo(user) {
+  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${user}&api_key=${API_KEY}&limit=1&format=json`;
+  const response = await fetch(url);
+  const json = await response.json();
+  const track = json.recenttracks.track[0];
+  return { user: json.recenttracks["@attr"].user, artist: track.artist["#text"], name: track.name, image: track.image[track.image.length - 1]["#text"], playing: ((typeof (track["@attr"]) != "undefined") ? true : false) };
+}
+
+
+
+async function handleRequest(request) {
+  const { pathname } = new URL(request.url);
+  const split = pathname.split("/");
+  const path = split[1].toLowerCase();
+  const user = String(split[2]);
+  if (path == "api") {
+    return new Response(((user.length != 0 && user != "undefined") ?
+      JSON.stringify(await getSongInfo(user)) : JSON.stringify({ "error": "Please specify your username; example: /api/i0l" })),
+      initJson);
+  }
+
+  if (path == "user") {
+    return new Response(((user.length != 0 && user != "undefined") ?
+      html.info(await getSongInfo(user)) : html.error("Please specify your Last.fm username")),
+      initHtml);
+  }
+
+  return new Response(html.main(), initHtml);
+}
+
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    handleRequest(event.request).catch(
+      (err) => new Response(err.stack, { status: 500 })
+    )
+  );
+});
